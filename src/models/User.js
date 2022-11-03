33932004ec232bc49595297fb16d9e42764d7007
Matheus Pereira <matheusdimmy@hotmail.com>
2022-11-03 18:57:34 -0300

    feat: Model User criado

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'users',
+  });
+
+  return User;
+}
+
+module.exports = User;
